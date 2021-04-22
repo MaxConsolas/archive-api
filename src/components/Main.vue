e2ce99f0bb9de425940f704ea704fd7092265c20
Consolas <sandakov2013@yandex.ru>
2021-04-22 23:00:28 +0500

    setting up

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout>
+      <v-text-field
+        label="Введите запрос"
+        placeholder="Введите сдесь ваш запрос" 
+        outlined
+      ></v-text-field>
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout>
+  <v-toolbar dark color="primary">
+    <v-toolbar-title class="white--text">{{ appName }}</v-toolbar-title>
+  </v-toolbar>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      appName: process.env.VUE_APP_NAME,
+    }
+  },
+  methods: {
+    test() {
+      console.log(process.env);
+    },
+  },
+  created: function() {
+    console.log(process.env);
+  },
+}
+</script>
+
+<style>
+
+</style>
